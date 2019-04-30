4d0aaa9fcdb6f5027085d275a4f68a56dfa3fef3
Daniela Barrientos <daniela@MacBook-Air-de-Daniela.local>
2019-04-30 15:49:33 -0500

    Adding header

---
+++ src/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles/App.scss';
+
+function Header() {
+    return (
+        <div className="App-header">
+            <div className="header-logo">Logo goes here</div>
+            <div className="header-navigation">
+                <a href="#about">About</a>
+                <div className="search-container">
+                    <form action="">
+                        <button className="search-button" type="submit"></button>
+                        <input className="search-bar" type="text" placeholder="Find article..." name="search"></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
