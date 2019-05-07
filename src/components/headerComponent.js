9926f7431425d19935f80e5249ebcafdce5d29cf
Daniela Barrientos <daniela@MacBook-Air-de-Daniela.local>
2019-05-07 16:55:21 -0500

    Header component quick fix

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';;
+import React, {Component} from 'react';
 import '../styles/App.scss';
 
 class HeaderComponent extends Component{
@@ -11,9 +11,7 @@ class HeaderComponent extends Component{
                     <div className="search-container">
                         <form action="">
                             <button className="search-button" type="submit"></button>
-                            <input className="search-bar" type="text" placeholder="Find grocery item..." name="search">
-                                {/*TODO Implement search bar filtering through products*/}
-                            </input>
+                            <input className="search-bar" type="text" placeholder="Find grocery item..." name="search"></input>
                         </form>
                     </div>
                 </div>
